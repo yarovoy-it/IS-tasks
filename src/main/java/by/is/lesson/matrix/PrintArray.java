278fb2f0fe37ecfa51de579957ac51191f3239bd
YarovoyYra <yarovoy1000@gmail.com>
2020-02-19 22:26:48 +0300

    print array as pyramid

--- src/main/java/by/is/lesson/matrix/PrintArray.java
+++ src/main/java/by/is/lesson/matrix/PrintArray.java
@@ -2,6 +2,29 @@ package by.is.lesson.matrix;
 
 public class PrintArray {
 
+    public static int[][] collectPyramidStructure(int[][] array) {
+        int pirSize = array.length + array[0].length;
+        int[][] pyramid = new int[pirSize][pirSize];
+        for (int row = 0; row < pyramid.length; row++) {
+            for (int col = 0; col < pyramid[row].length; col++) {
+                int columns = 0;
+                int rows = 0;
+                if (row < pirSize / 2) {
+                    rows = Math.min(row, col);
+                } else {
+                    rows = Math.min(pyramid.length - 1 - row, col);
+                }
+                if (col < pirSize / 2) {
+                    columns = Math.min(row, col);
+                } else {
+                    columns = Math.min(row, pirSize - 1 - col);
+                }
+                pyramid[row][col] = Math.min(columns, rows);
+            }
+        }
+        return pyramid;
+    }
+
     public static void printArray(int[][] array) {
         for (int col = 0; col < array.length; col++) {
             for (int row = 0; row < array[col].length; row++) {
@@ -10,17 +33,6 @@ public class PrintArray {
                 System.out.print(array[col][row] + " ");
             }
             System.out.println("");
-        }
-    }
-
-    public static void printArrayFirst(int[][] array) {
-        for (int mainArray = 0; mainArray < array.length; mainArray++) {
-            for (int subArray = 0; subArray < array[mainArray].length; subArray++) {
-                System.out.print(array[mainArray][mainArray] + " ");
-                array[mainArray][subArray] = subArray;
-                System.out.print(array[mainArray][mainArray] + " ");
-            }
-            System.out.println(array[mainArray][mainArray]);
         }
     }
 
--- src/main/java/by/is/lesson/matrix/Runner.java
+++ src/main/java/by/is/lesson/matrix/Runner.java
@@ -1,16 +1,22 @@
 package by.is.lesson.matrix;
 
-import static by.is.lesson.matrix.PrintArray.*;
+import java.util.Arrays;
 
+import static by.is.lesson.matrix.PrintArray.collectPyramidStructure;
 
 
 public class Runner {
 
     public static void main(String[] args) {
 
-//        printMatrix(3, 4);
-
-        int[][] array = new int[3][4];
-        printArrayFirst(array);
+        int[][] matrix = new int[3][4];
+        int[][] array = collectPyramidStructure(matrix);
+        StringBuilder sb2 = new StringBuilder();
+        for (int[] tempArray : array) {
+            sb2.append(Arrays.toString(tempArray)).append('\n');
+        }
+        System.out.println(sb2);
     }
+
 }
+

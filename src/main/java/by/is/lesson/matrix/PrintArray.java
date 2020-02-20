98e9bfc0006a2450a83659635e85f3fb37ddfaed
yarovoy <yarovoy1000@gmail.com>
2020-02-20 16:44:31 +0300

    refactor pyramid printer from array

--- src/main/java/by/is/lesson/matrix/PrintArray.java
+++ src/main/java/by/is/lesson/matrix/PrintArray.java
@@ -25,15 +25,34 @@ public class PrintArray {
         return pyramid;
     }
 
-    public static void printArray(int[][] array) {
-        for (int col = 0; col < array.length; col++) {
-            for (int row = 0; row < array[col].length; row++) {
-                System.out.print(array[col][col - col] + " ");
-                array[col][row] = col;
-                System.out.print(array[col][row] + " ");
+    public static void printPyramid(int[][] array) {
+        int arraySize = array.length;
+        int[][] fullArray = printArrayColumn(array);
+        for (int row = 0; row < arraySize; row++) {
+            for (int col = 0; col < arraySize; col++) {
+                if (row < arraySize / 2) {
+                    array[row][col] = Math.min(row, fullArray[row][col]);
+                } else {
+                    array[row][col] = Math.min((arraySize - 1 - row), fullArray[row][col]);
+                }
+                System.out.print(array[row][col]);
             }
             System.out.println("");
         }
+    }
+
+    private static int[][] printArrayColumn(int[][] array) {
+        int arraySize = array.length;
+        for (int row = 0; row < arraySize; row++) {
+            for (int col = 0; col < arraySize; col++) {
+                if (row < arraySize / 2) {
+                    array[col][row] = row;
+                } else {
+                    array[col][row] = (arraySize - 1 - row);
+                }
+            }
+        }
+        return array;
     }
 
 
--- src/main/java/by/is/lesson/matrix/Runner.java
+++ src/main/java/by/is/lesson/matrix/Runner.java
@@ -1,21 +1,26 @@
 package by.is.lesson.matrix;
 
-import java.util.Arrays;
-
-import static by.is.lesson.matrix.PrintArray.collectPyramidStructure;
+import static by.is.lesson.matrix.PrintArray.printPyramid;
 
 
 public class Runner {
 
     public static void main(String[] args) {
 
+        int[][] wave = new int[7][7];
         int[][] matrix = new int[3][4];
-        int[][] array = collectPyramidStructure(matrix);
-        StringBuilder sb2 = new StringBuilder();
-        for (int[] tempArray : array) {
-            sb2.append(Arrays.toString(tempArray)).append('\n');
-        }
-        System.out.println(sb2);
+//        int[][] array = collectPyramidStructure(matrix);
+//        StringBuilder sb2 = new StringBuilder();
+//        for (int[] tempArray : array) {
+//            sb2.append(Arrays.toString(tempArray)).append('\n');
+//        }
+//        System.out.println(sb2);
+
+
+//        printArray(matrix);
+
+//        System.out.println();
+        printPyramid(wave);
     }
 
 }
